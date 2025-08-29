36b2f15dc67da4cf5a7adcc24f3ca168ecf61576
JhonJurielGulane599 <jhongulane126@gmail.com>
2025-08-29 16:57:37 +0800

    commit 1

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>HELLOOOOOOOOOOO GOD IS GOOD ALL THE TIME!</Text>
+    </View>
+  );
+}
